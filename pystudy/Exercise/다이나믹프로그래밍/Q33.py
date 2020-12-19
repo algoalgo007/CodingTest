231ed757068e9d4127b206bd9730f40e0fedef13
KiBumYoon <62062154+YoonKiBum@users.noreply.github.com>
2020-12-19 15:12:11 +0900

    Create Q33.py

---
+++ pystudy/Exercise/다이나믹프로그래밍/Q33.py
@@ -0,0 +1,15 @@
+n = int(input())
+data = [0]
+ans = [0]*(2+n)
+for i in range(1, n+1):
+  t, p = map(int, input().split())
+  data.append((i, t, p))
+
+for i in range(1, n+1):
+  day, t, p = data[i]
+  if day + t <= n+1:
+    ans[i] += p
+    for j in range(day+t, n+1):
+      ans[j] = max(ans[day], ans[j])
+
+print(max(ans))
