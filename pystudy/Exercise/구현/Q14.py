62a9f5a14b447373d534e008c09ee4ec7240a7ac
KiBumYoon <62062154+YoonKiBum@users.noreply.github.com>
2021-01-06 17:40:56 +0900

    Create Q14.py

---
+++ pystudy/Exercise/구현/Q14.py
@@ -0,0 +1,23 @@
+from itertools import permutations
+
+def solution(n, weak, dist):
+    length = len(weak)
+    result = len(dist) + 1
+    
+    for i in range(length):
+        weak.append(weak[i] + n)
+    for start in range(length):
+        for friends in list(permutations(dist, len(dist))):
+            count = 1
+            position = weak[start] + friends[count - 1]
+            for index in range(start, start + length):
+                if position < weak[index]:
+                    count += 1
+                    if count > len(dist):
+                        break
+                    position = weak[index] + friends[count - 1]
+            result = min(result, count)
+    if result > len(dist):
+        return -1
+    else:
+        return result
