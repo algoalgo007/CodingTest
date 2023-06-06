1f5ba108ad13b799a21db7aae2bdba7b47157a22
KiBumYoon <62062154+YoonKiBum@users.noreply.github.com>
2023-06-06 21:07:46 +0900

    Create Bfs.java

---
+++ java/Bfs.java
@@ -0,0 +1,70 @@
+/*
+[output]
+1 2 3 8 7 4 5 6
+ */
+import java.util.*;
+import java.io.*;
+
+public class Bfs {
+    public static boolean[] visited = new boolean[9];
+    public static ArrayList<ArrayList<Integer>> graph = new ArrayList<ArrayList<Integer>>();
+
+    public static void bfs(int start) {
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(start);
+        visited[start] = true;
+        while(!q.isEmpty()) {
+            int v = q.poll();
+            System.out.print(v + " ");
+            for(int i = 0; i < graph.get(v).size(); i++) {
+                int y = graph.get(v).get(i);
+                if(!visited[y]) {
+                    visited[y] = true;
+                    q.offer(y);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        for(int i = 0; i < 9; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        // 노드 1에 연결된 노드 정보 저장
+        graph.get(1).add(2);
+        graph.get(1).add(3);
+        graph.get(1).add(8);
+
+        // 노드 2에 연결된 노드 정보 저장
+        graph.get(2).add(1);
+        graph.get(2).add(7);
+
+        // 노드 3에 연결된 노드 정보 저장
+        graph.get(3).add(1);
+        graph.get(3).add(4);
+        graph.get(3).add(5);
+
+        // 노드 4에 연결된 노드 정보 저장
+        graph.get(4).add(3);
+        graph.get(4).add(5);
+
+        // 노드 5에 연결된 노드 정보 저장
+        graph.get(5).add(3);
+        graph.get(5).add(4);
+
+        // 노드 6에 연결된 노드 정보 저장
+        graph.get(6).add(7);
+
+        // 노드 7에 연결된 노드 정보 저장
+        graph.get(7).add(2);
+        graph.get(7).add(6);
+        graph.get(7).add(8);
+
+        // 노드 8에 연결된 노드 정보 저장
+        graph.get(8).add(1);
+        graph.get(8).add(7);
+
+        bfs(1);
+    }
+}
